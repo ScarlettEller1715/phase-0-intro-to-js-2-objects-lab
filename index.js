dcd2c5f580b8d9f06c8fc7e1d6bc33157ab0a222
Scarlett Eller <rq6715@gmail.com>
2022-01-16 19:08:55 -0500

    commit tree 932c2c5a870becfc90f813668884a7ef68427d7c

--- index.js
+++ index.js
@@ -4,25 +4,64 @@ const employee = {
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const object = {...employee};
-    object["name"] = 'Sam';
-    object["streetAddress"] = '11 Broadway';
-    return object;
-};
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee.name = 'Sam';
-    employee.streetAddress = '12 Broadway';
-    return employee;
-};
-
-function deleteFromEmployeeByKey(employee, key, value) {
-    const object = {...employee};
-    delete object.name;
-    return object;
+    const newList = {...employee}
+    newList['name'] = 'Sam';
+    newList['streetAddress'] = '11 Broadway'
+    return newList
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee['name'] = 'Sam'
+    employee['streetAddress'] = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const anotherList = {...employee};
+    delete anotherList.name;
+    return anotherList;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee.name;
     return employee;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function updateEmployeeWithKeyAndValue(employee, key, value) {
+    //const object = {...employee};
+    //object["name"] = 'Sam';
+    //object["streetAddress"] = '11 Broadway';
+    //return object;
+//};
+
+//function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    //employee.name = 'Sam';
+   // employee.streetAddress = '12 Broadway';
+   // return employee;
+//};
+
+//function deleteFromEmployeeByKey(employee, key, value) {
+    //const object = {...employee};
+    //delete object.name;
+    //return object;
+//}
+
+//function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+//    delete employee.name;
+ //   return employee;
+//}
+
+
